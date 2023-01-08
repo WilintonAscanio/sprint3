8f6b10c3da1bccd6236dbc8159a751c8c810ac90
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-01-08 00:07:35 -0500

    Se agregan los filtros y otras funciones

--- main.js
+++ main.js
@@ -1,48 +1,4 @@
-const predios = [
-    {
-        id: 'card1',
-        type:'House',
-        city:'Medellin Colombia',
-        imgBackground: './images/PropertyImage.png',
-        direccion: 'Barrio el poblado'
-    },
-    {
-        type:'Apartment',
-        id: 'card2',
-        city:'Los Angeles',
-        imgBackground: './images/PropertyImage2.png',
-        direccion:'ChinaTown Center'
-    },
-    {
-        type:'House',
-        id: 'card3',
-        city:'New York',
-        imgBackground: './images/PropertyImage3.png',
-        direccion:'Street 48 #23B-C'
-    },
-    {
-        type:'House',
-        id: 'card4',
-        city:'El Cabo',
-        imgBackground: "./images/PropertyImage4.png",
-        direccion:'Diagonal sexta, via a la playa'
-    },
-    {
-        type:'Apartment',
-        id: 'card5',
-        city:'Bogotá',
-        imgBackground: './images/PropertyImage5.png',
-        direccion:'Calle 30 #58C-22'
-    },
-    {
-        type:'Apartment',
-        id: 'card6',
-        city:'Ciudad de México',
-        imgBackground: './images/PropertyImage6.png',
-        direccion:'Barrio los Mariachis, Sur'
-    }
-
-]
+let predios = []
 const agents = [
     {
         img: './images/agent1.png',
@@ -59,6 +15,7 @@ const agents = [
 ]
 
 const containerCards = document.getElementById('mainContainer');
+
 const modalContainer = document.getElementById('modal__container')
 
 const cardAgentsCards = document.getElementById('cardAgents');
@@ -71,7 +28,42 @@ const modal = document.querySelector('.modal');
 
 const response = fetch('api.json');
 
+const btnProperties = document.getElementById('btn3');
 
+const select1 = document.getElementById('select1');
+
+const select2 = document.getElementById('select2');
+
+const userSearch = document.getElementById('userSearch');
+
+const allProperties = document.getElementById('btnBottom');
+
+const lessProperties = document.getElementById('lessProperties');
+
+
+
+
+userSearch.addEventListener('input', (e) => {
+    let arrayFiltered = predios.filter(key => key.place.toLowerCase().startsWith(e.target.value))
+
+    renderCards(arrayFiltered);
+})
+
+btnProperties.addEventListener('click', () => {
+
+    if (select1.value == "" || select2.value == "") {
+
+        alert('Seleccione ambas opciones')
+
+    } else {
+        let opc1 = select1.value == "1" ? "APARTMENT" : "HOUSE";
+        let opc2 = select2.value == "1" ? true : false;
+
+        let arrayFilter = predios.filter(house => house.apartment == opc1 && house.status == opc2);
+        renderCards(arrayFilter);
+    }
+
+})
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
@@ -85,57 +77,57 @@ window.addEventListener('scroll', () => {
     }
 });
 
-const renderCards = () => {
-    predios.forEach(predio => {
-        containerCards.innerHTML += `
-        <main>
-        <section class="card" id="${predio.id}">
-        <div class="top" style="background-image: url(${predio.imgBackground});">
-            <div class="infoTop">
-                <p class="infoImg" id="textHouse">${predio.type}</p>
-                <p class="infoImg" id="textSale">FOR SALE</p>
+const renderCards = (predios) => {
+    containerCards.innerHTML = "";
+    if (predios.length == 0) {
+        containerCards.innerHTML = `
+        <h1 style ="text-align: center"> Whithout Results</h1>`
+    }
+    else {
+        predios.forEach((predio) => {
+            containerCards.innerHTML += `
+            <section class="card" id="${predio.id}">
+            <div class="top" style="background-image: url(${predio.url});">
+                <div class="infoTop">
+                    <p class="infoImg" id="textHouse">${predio.apartment}</p>
+                    <p class="infoImg ${predio.status ? 'forSale': 'sold'}" id="textSale">${predio.status == true ? 'FOR SALE' : 'SOLD'}</p>
+                </div>
+                <div class="infoPrice">
+                    <p class="infoImg" id="price"><strong>$${predio.priceHouse}</strong> </p>
+                </div>
             </div>
-            <div class="infoPrice">
-                <p class="infoImg" id="price"><strong>$320,000.00</strong> </p>
+            <div class="infoBottom">
+                <a id="city">${predio.place}</a>
+                <p>${predio.PlaceDescription}</p>
+                <div class="agent">
+                    <figure class="infoAgent">
+                        <img src="./images/agent-1-3-60x60.png" alt="icon" id="agentIcon">
+                        <a id="nameAgent">${predio.nameLessee}</a>
+                    </figure>
+                    <a id="months">${predio.time}</a>
+                </div>
+                <div class="iconsBottom">
+                    <figure>
+                        <img src="./images/Area Icon.png" alt="areaIcon">
+                        <a><strong>${predio.area}</strong> Sq Ft</a>
+                    </figure>
+                    <figure class="iconsBottomRight">
+                        <img src="./images/Garage Icon.png" alt="garageIcon">
+                        <a>${predio.garage}</a>
+                        <img src="./images/Bathroom Icon.png" alt="bathroomsIcon" class="iconRight">
+                        <a>${predio.bathroom}</a>
+                        <img src="./images/Bedroom Icon.png" alt="bedroomsIcon" class="iconRight">
+                        <a>${predio.beedroom}</a>
+                    </figure>
+                </div>
             </div>
-        </div>
+        </section>  
+        `
+        });
 
 
-        <div class="infoBottom">
-            <a id="city">${predio.city}</a>
-            <p>${predio.direccion}</p>
-            <div class="agent">
-                <figure class="infoAgent">
-                    <img src="./images/agent-1-3-60x60.png" alt="icon" id="agentIcon">
-                    <a id="nameAgent"> Hellen Mattis</a>
-                </figure>
-                <a id="months"> 4 months ago</a>
-            </div>
-            <div class="iconsBottom">
-                <figure>
-                    <img src="./images/Area Icon.png" alt="areaIcon">
-                    <a><strong>2400</strong> Sq Ft</a>
-                </figure>
-                <figure class="iconsBottomRight">
-                    <img src="./images/Garage Icon.png" alt="garageIcon">
-                    <a>2</a>
-                    <img src="./images/Bathroom Icon.png" alt="bathroomsIcon" class="iconRight">
-                    <a>3</a>
-                    <img src="./images/Bedroom Icon.png" alt="bedroomsIcon" class="iconRight">
-                    <a>3</a>
-                </figure>
-            </div>
-        </div>
+    }
 
-
-
-    </section>
-
-        </main>
-       
-    `
-
-    });
 }
 const renderCardAgent = () => {
     agents.forEach(agent => {
@@ -157,98 +149,86 @@ const renderCardAgent = () => {
         </section>`
 
     })
-
 }
 
-renderCards();
+
+
+
+
+response.then((result) => {
+    return result.json();
+}).then((data) => {
+    predios = data;
+    let minPredios = predios.slice(0, 6)
+    renderCards(minPredios);
+})
+
 renderCardAgent();
 
-
-
-const card1 = document.getElementById('card1');
-const card2 = document.getElementById('card2');
-const card3 = document.getElementById('card3');
-const card4 = document.getElementById('card4');
-const card5 = document.getElementById('card5');
-const card6 = document.getElementById('card6');
-const closeModal = document.querySelector('.modal__close');
-
-
-card1.addEventListener('click', (e)=>{
-    e.preventDefault();
-    document.getElementById('allModal').innerHTML=`
-    <div class="allModal" id="allModal">
-                <img src="./images/PropertyImage.png" class="modal__img" id="modal__img">
-                <p id="p1Modal">$320,000.00 </p>
-                <p id="p2Modal">FOR SALE</p>
-                <p id="p3Modal">Medellin Colombia</p>
-                <p id="p4Modal">Barrio el Poblado</p>
-            </div>`
-    modal.classList.add('modal--show');
+containerCards.addEventListener('click', (e) => {
+    if (e.target.classList.contains('top')) {
+        let arrayModal = predios.filter(predio => predio.id == e.path[1].id)
+        arrayModal.forEach(e => {
+            modalContainer.innerHTML = `
+            <div class="allModal" id="allModal">
+            <img src="${e.url}" class="modal__img" id="modal__img">
+            <p id="p1Modal">$${e.priceHouse} </p>
+            <p id="p2Modal">${e.status == true ? 'FOR SALE' : 'SOLD'}</p>
+            <p id="p3Modal">${e.place}</p>
+            <p id="p4Modal">${e.PlaceDescription}</p>
+        </div>
+        <div class="rightModal">
+            <figure>
+                <div>
+                    <img src="./images/Area Icon.png" alt="areaIcon">
+                    <a><strong>${e.area}</strong> Sq Ft</a>
+                </div>
+                <div>
+                    <img src="./images/Garage Icon.png" alt="garageIcon" class="figureModal">
+                    <a>${e.garage}</a>
+                </div>
+                <div>
+                    <img src="./images/Bathroom Icon.png" alt="bathroomsIcon" class="figureModal">
+                    <a>${e.bathroom}</a>
+                </div>
+                <div>
+                    <img src="./images/Bedroom Icon.png" alt="bedroomsIcon" class="figureModal">
+                    <a>${e.beedroom}</a>
+                </div>
+            </figure>
+            <div class="agentModal">
+                <figure class="infoAgentModal">
+                    <img src="./images/agent-1-3-60x60.png" alt="icon" id="agentIconModal">
+                    <a id="nameAgentModal">${e.nameLessee}</a>
+                </figure>
+                <a id="monthsModal">${e.time}</a>
+            </div>
+        </div>
+        <a href="#" class="modal__close">Cerrar</a>
+    `
+            modal.classList.add('modal--show');
+        })
+    }
 })
 
-card2.addEventListener('click', (e)=>{
+modalContainer.addEventListener('click', (e) => {
     e.preventDefault();
-    document.getElementById('allModal').innerHTML=`
-    <div class="allModal" id="allModal">
-                <img src="./images/PropertyImage2.png" class="modal__img" id="modal__img">
-                <p id="p1Modal">$320,000.00 </p>
-                <p id="p2Modal">FOR SALE</p>
-                <p id="p3Modal">Los Angeles</p>
-                <p id="p4Modal">ChinaTown Center</p>
-            </div>`
-    modal.classList.add('modal--show');
+    if (e.target.classList.contains('modal__close')) {
+        modal.classList.remove('modal--show');
+    }
 })
-card3.addEventListener('click', (e)=>{
-    e.preventDefault();
-    document.getElementById('allModal').innerHTML=`
-    <div class="allModal" id="allModal">
-                <img src="./images/PropertyImage3.png" class="modal__img" id="modal__img">
-                <p id="p1Modal">$320,000.00 </p>
-                <p id="p2Modal">FOR SALE</p>
-                <p id="p3Modal">New York</p>
-                <p id="p4Modal">Street 48 #23B-C</p>
-            </div>`
-    modal.classList.add('modal--show');
-})
-card4.addEventListener('click', (e)=>{
-    e.preventDefault();
-    document.getElementById('allModal').innerHTML=`
-    <div class="allModal" id="allModal">
-                <img src="./images/PropertyImage4.png" class="modal__img" id="modal__img">
-                <p id="p1Modal">$320,000.00 </p>
-                <p id="p2Modal">FOR SALE</p>
-                <p id="p3Modal">El Cabo</p>
-                <p id="p4Modal">Diagonal sexta, via a la playa</p>
-            </div>`
-    modal.classList.add('modal--show');
-})
-card5.addEventListener('click', (e)=>{
-    e.preventDefault();
-    document.getElementById('allModal').innerHTML=`
-    <div class="allModal" id="allModal">
-                <img src="./images/PropertyImage5.png" class="modal__img" id="modal__img">
-                <p id="p1Modal">$320,000.00 </p>
-                <p id="p2Modal">FOR SALE</p>
-                <p id="p3Modal">Bogotá</p>
-                <p id="p4Modal">Calle 30 #58C-22</p>
-            </div>`
-    modal.classList.add('modal--show');
-})
-card6.addEventListener('click', (e)=>{
-    e.preventDefault();
-    document.getElementById('allModal').innerHTML=`
-    <div class="allModal" id="allModal">
-                <img src="./images/PropertyImage6.png" class="modal__img" id="modal__img">
-                <p id="p1Modal">$320,000.00 </p>
-                <p id="p2Modal">FOR SALE</p>
-                <p id="p3Modal">Ciudad de México</p>
-                <p id="p4Modal">Barrio los Mariachis, Sur</p>
-            </div>`
-    modal.classList.add('modal--show');
-});
 
-closeModal.addEventListener('click', (e)=>{
-    e.preventDefault();
-    modal.classList.remove('modal--show');
+allProperties.addEventListener('click', () => {
+  renderCards(predios);
+  allProperties.classList.add('hidden');
+  lessProperties.classList.remove('hidden')
+
+lessProperties.addEventListener('click', () => {
+    let minPredios = predios.slice(0, 6)
+    renderCards(minPredios);
+    allProperties.classList.remove('hidden');
+    lessProperties.classList.add('hidden')
+  })
 })
+
+
